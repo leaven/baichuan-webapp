39953aa023208ce894f2882e0905784ff1e1d55c
welllee <welllee@diditaxi.com.cn>
2016-08-19 19:13:38 +0800

    update ui

--- index/components/home/postList/postList.jsx
+++ index/components/home/postList/postList.jsx
@@ -3,13 +3,13 @@ import iScroll from 'iscroll/build/iscroll-lite';
 
 class PostList extends React.Component{
 	componentDidMount() {
-		const mainScroll = new iScroll('.main', {
+		this.mainScroll = new iScroll('.main', {
 			mouseWheel: true,
             click: true
         });
         let pn = 1;
         const me = this;
-        mainScroll.on('scrollEnd', function () {
+        this.mainScroll.on('scrollEnd', function () {
         	if(this.y < 0) {
         		if(!me.props.isLastPage) {
         			me.props.getPosts(++pn);
@@ -18,6 +18,7 @@ class PostList extends React.Component{
         });
 	}
 	render() {
+		const me = this;
 		return (
 			<div className="main">
 				<ul className="post-list">
@@ -38,18 +39,25 @@ class PostList extends React.Component{
 						}) 
 					}
 					{(()=>{
-						if(this.props.getNextPage) {
+						if(me.props.getNextPage) {
 							return (
 								<div className="load-nextpage">加载中...</div>
 							)
 						}
-						if(this.props.isLastPage) {
+						if(me.props.isLastPage) {
 							return (
 								<div className="last-page">没有更多啦~</div>
 							)
 						}
+
 					})()}
+
 				</ul>
+				{(()=>{
+					if(this.mainScroll) {
+						this.mainScroll.refresh();
+					}
+				})()}
 			</div>
 		)
 	}
